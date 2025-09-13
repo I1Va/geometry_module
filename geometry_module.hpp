d49a5282f3baa5a741d1bc379d8d09724c986e06
Abrutin Ivan Dmitrievich <mai.abrutinid@gmail.com>
2025-09-13 19:01:53 +0300

    add gm_vectors

--- geometry_module.hpp
+++
@@ -1,6 +0,0 @@
-#ifndef GEOMETRY_MODULE_HPP
-#define GEOMETRY_MODULE_HPP
-
-#include "vectors.hpp"
-
-#endif // GEOMETRY_MODULE_HPP
---
+++ inc/gm_vector.hpp
@@ -0,0 +1,68 @@
+#ifndef GM_VECTOR_HPP
+#define GM_VECTOR_HPP
+
+#include <cmath>
+#include <iostream>
+
+
+template<typename T, std::size_t N>
+class gm_vector {};
+
+
+template<typename T>
+gm_vector<T, 3> cord_mul(const gm_vector<T, 3> &a, const gm_vector<T, 3> &b);
+
+gm_vector<double, 3> cord_pow(const gm_vector<double, 3> &a, const double pow_val);
+gm_vector<double, 3> get_ortogonal(gm_vector<double, 3> a, gm_vector<double, 3> b);
+
+
+template<typename T>
+class gm_vector<T, 2> {
+    T x, y;
+    T len2;
+public:
+    gm_vector(T x = 0, T y = 0);
+    gm_vector<T, 2> operator+(const gm_vector<T, 2> &other) const;
+    gm_vector<T, 2> operator-(const gm_vector<T, 2> &other) const;
+    gm_vector<T, 2> operator*(const T scalar) const;
+    gm_vector<T, 2> operator+=(const gm_vector<T, 2> &other);
+    gm_vector<T, 2> operator-=(const gm_vector<T, 2> &other);
+    gm_vector<T, 2> operator*=(const T scalar);
+    gm_vector<double, 2> rotate(const double radians) const;
+    T dot(const gm_vector<T, 2> &other) const;
+    T get_len2();
+    T get_x() const;
+    T get_y() const;
+};
+
+
+
+template<typename T>
+class gm_vector<T, 3> {
+    T x, y, z;
+    T len2;
+public:
+    gm_vector(T x = 0, T y = 0, T z = 0);
+    gm_vector<T, 3> operator+(const gm_vector<T, 3> &other) const;
+    gm_vector<T, 3> operator-(const gm_vector<T, 3> &other) const;
+    gm_vector<T, 3> operator*(const T scalar) const;
+    gm_vector<double, 3> operator!();
+    gm_vector<T, 3> operator+=(const gm_vector<T, 3> &other);
+    gm_vector<T, 3> operator-=(const gm_vector<T, 3> &other);
+    gm_vector<T, 3> operator*=(const T scalar);
+    T get_len2();
+    T dot(const gm_vector<T, 3> &other) const;
+    T get_x() const;  
+    T get_y() const;
+    T get_z() const; 
+    gm_vector<T, 3> clamp(const T min, const T max);
+
+    friend gm_vector<double, 3> cord_pow(const gm_vector<double, 3> &a, const double pow_val);
+    gm_vector<T, 3> cord_mul(const gm_vector<T, 3> &a, const gm_vector<T, 3> &b);
+};
+
+template<typename T>
+std::ostream &operator<<(std::ostream &stream, const gm_vector<T, 3> &vector);
+
+
+#endif // GM_VECTOR_HPP
